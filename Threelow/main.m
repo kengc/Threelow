74d4b2cdb0a88535b5251768be80db955527b14a
kevin Cleathero <kengc@hotmail.com>
2017-05-31 14:39:13 -0700

    project creation

---
+++ Threelow/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Threelow
+//
+//  Created by Kevin Cleathero on 2017-05-31.
+//  Copyright © 2017 Kevin Cleathero. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
